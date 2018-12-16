add50ea53e5aa38206a4a355cfa07eae580335a9
Ashar Khan <0ashar0786@gmail.com>
2018-12-16 23:39:51 +0530

    competition soln

---
+++ Java/DefiniteGame.java
@@ -0,0 +1,16 @@
+package codeForces;
+
+import java.util.Scanner;
+
+public class DefiniteGame {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		long v = in.nextLong();
+		if (v==2)
+			System.out.println(v);
+		else
+			System.out.println(1);
+	}
+
+}
