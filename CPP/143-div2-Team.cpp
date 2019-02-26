2433dfef8e196d9ecbb647c025fcddf9c7c2c2b0
Dhruv Kunjumon <dhruvsci30@gmail.com>
2019-02-27 01:32:08 +0530

    solved!

--- CPP/143-div2-Team.cpp
+++ CPP/143-div2-Team.cpp
@@ -1,1 +1,18 @@
-#include <bits/ >
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, count = 0; cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int s = 0, t;
+        for (int j = 0; j < 3; j++) {
+            cin >> t;
+            s += t;
+        }
+        count += (s >= 2);
+    }
+    cout << count << endl;
+    return 0;
+}
